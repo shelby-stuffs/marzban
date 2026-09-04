7b464b8f2f46193fce0e2eb7d36c2c5086fb5892
Alexey Shelby <shelbyhell@proton.me>
2026-09-04 10:44:20 +0400

    feat(wireguard): expose subscription renderer

--- app/wireguard/__init__.py
+++ app/wireguard/__init__.py
@@ -1,11 +1,14 @@
 from .allocator import WireGuardNamespace, allocate_peer_ips, build_namespaces
 from .config import WireGuardConfig
 from .models import WireGuardPeerSettings
+from .subscription import WireGuardSubscription, render_wireguard_client_config
 
 __all__ = [
     "WireGuardConfig",
     "WireGuardNamespace",
     "WireGuardPeerSettings",
+    "WireGuardSubscription",
     "allocate_peer_ips",
     "build_namespaces",
+    "render_wireguard_client_config",
 ]
