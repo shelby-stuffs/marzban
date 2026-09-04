f561f3c7e518d3ea3809513551d9fd3f00da25b8
Alexey Shelby <shelbyhell@proton.me>
2026-09-04 10:41:24 +0400

    feat(wireguard): expose peer allocation API

--- app/wireguard/__init__.py
+++ app/wireguard/__init__.py
@@ -1,3 +1,11 @@
+from .allocator import WireGuardNamespace, allocate_peer_ips, build_namespaces
 from .config import WireGuardConfig
+from .models import WireGuardPeerSettings
 
-__all__ = ["WireGuardConfig"]
+__all__ = [
+    "WireGuardConfig",
+    "WireGuardNamespace",
+    "WireGuardPeerSettings",
+    "allocate_peer_ips",
+    "build_namespaces",
+]
